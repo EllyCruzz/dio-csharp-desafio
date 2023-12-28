e1857204e328ffd15cb12b3d315fd21af95cbfd6
EllyCruzz <serena_ellen@hotmail.com>
2023-12-28 14:32:35 -0300

    Tira comentário desnecessário

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -15,7 +15,7 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
+          
             
             Console.Write("Digite a placa do veículo para estacionar: ");
             string placa = Console.ReadLine();
